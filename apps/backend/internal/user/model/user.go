80ad36fa76583626e6363c1dade70040a567ee20
NexusXian <xyhcatrl@gmail.com>
2026-08-30 02:03:43 +0800

    chore(api):added more tags for user model

--- apps/backend/internal/user/model/user.go
+++ apps/backend/internal/user/model/user.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type User struct {
-	Id        int       `gorm:"primaryKey" json:"id"`
-	Username  string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	Avatar    string    `json:"avatar"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID           uint      `gorm:"primaryKey" json:"id"`
+	Username     string    `gorm:"size:50;not null" json:"username"`
+	Email        string    `gorm:"size:255;uniqueIndex;not null" json:"email"`
+	PasswordHash string    `gorm:"not null" json:"-"`
+	Avatar       string    `json:"avatar"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 
